d8c06490dd802272484277cfcd3cfcf74235d069
Shumpei Urabe <urabe@hack.ne.jp>
2018-03-12 18:18:18 +0900

    Fix

--- build.rs
+++ build.rs
@@ -39,21 +39,11 @@ fn find_dir(default: &'static str, env_key: &'static str) -> PathBuf {
 }
 
 fn main() {
-<<<<<<< HEAD
     let root_include = find_dir("l-smash", "ROOT_INCLUDE_PATH");
     let codecs_include = find_dir("l-smash/codecs", "CODECS_INCLUDE_PATH");
-    let lsmash_include = find_dir("l-smash/l-smash", "CORE_INCLUDE_PATH");
+    let lsmash_include = find_dir("l-smash/lsmash", "CORE_INCLUDE_PATH");
     let importer_include = find_dir("l-smash/importer", "IMPORTER_INCLUDE_PATH");
 
-    //println!("cargo:rustc-link-lib=dylib={}", "lsmash");
-=======
-    let root_include = find_dir("lsmash", "ROOT_INCLUDE_PATH");
-    let codecs_include = find_dir("lsmash/codecs", "CODECS_INCLUDE_PATH");
-    let lsmash_include = find_dir("lsmash/lsmash", "CORE_INCLUDE_PATH");
-    let importer_include = find_dir("lsmash/importer", "IMPORTER_INCLUDE_PATH");
-
-    // println!("cargo:rustc-link-lib=dylib={}", "lsmash");
->>>>>>> a6f6979... first commit
 
     let builder = common_builder()
         .clang_arg(format!("-I{}", root_include.to_string_lossy()))
