274060fae14270bc1fd3c0e6329a935825cfe0e2
Shumpei Urabe <urabe@hack.ne.jp>
2018-03-19 14:57:54 +0900

    Fix build for lsmash

--- build.rs
+++ build.rs
@@ -1,57 +1,18 @@
 extern crate bindgen;
 
-use std::env;
-use std::fs::OpenOptions;
-use std::io::Write;
 use std::path::PathBuf;
 
-fn format_write(builder: bindgen::Builder, output: &str) {
-    let s = builder
-        .generate()
-        .unwrap()
-        .to_string()
-        .replace("/**", "/*")
-        .replace("/*!", "/*");
-
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open(output)
-        .unwrap();
-
-    let _ = file.write(s.as_bytes());
-}
-
-fn common_builder() -> bindgen::Builder {
-    bindgen::builder()
-        .raw_line("#![allow(dead_code)]")
-        .raw_line("#![allow(non_camel_case_types)]")
-        .raw_line("#![allow(non_snake_case)]")
-        .raw_line("#![allow(non_upper_case_globals)]")
-}
-
-fn find_dir(default: &'static str, env_key: &'static str) -> PathBuf {
-    match env::var_os(env_key) {
-        Some(val) => PathBuf::from(&val),
-        _ => PathBuf::from(default),
-    }
-}
-
 fn main() {
-    let root_include = find_dir("l-smash", "ROOT_INCLUDE_PATH");
-    let codecs_include = find_dir("l-smash/codecs", "CODECS_INCLUDE_PATH");
-    let lsmash_include = find_dir("l-smash/lsmash", "CORE_INCLUDE_PATH");
-    let importer_include = find_dir("l-smash/importer", "IMPORTER_INCLUDE_PATH");
+    println!("cargo:rustc-link-lib=lsmash");
 
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .blacklist_type("max_align_t")
+        .generate()
+        .expect("Unable to generate liblsmash bindings");
 
-    let builder = common_builder()
-        .clang_arg(format!("-I{}", root_include.to_string_lossy()))
-        .clang_arg(format!("-I{}", codecs_include.to_string_lossy()))
-        .clang_arg(format!("-I{}", lsmash_include.to_string_lossy()))
-        .clang_arg(format!("-I{}", importer_include.to_string_lossy()))
-        .header(root_include.join("lsmash.h").to_string_lossy());
-
-    // Manually fix the comment so rustdoc won't try to pick them
-    format_write(builder, "src/lsmash.rs");
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").expect("Environment variable `OUT_DIR' is not defined"));
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write liblsmash bindings");
 }
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include <lsmash.h>
